153b03d39056e59a2ece216fd18b5b5214506541
Antonio Alves <antonioaugusto.alves1981@gmail.com>
2022-10-08 23:02:06 -0300

    Iniciando o teste de autenticação dos usuarios cadastrados

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,1 +1,27 @@
-describe("User's authentication test", () => {});
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+let createUserUseCase: CreateUserUseCase;
+let autheticateUserUseCase: AuthenticateUserUseCase;
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+describe("User's authentication test", () => {
+    beforeEach(() => {
+        usersRepositoryInMemory = new UsersRepositoryInMemory();
+        createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+        autheticateUserUseCase = new AuthenticateUserUseCase(
+            usersRepositoryInMemory
+        );
+    });
+    it("Should be able to authenticate an user", async () => {
+        const user: ICreateUserDTO = {
+            name: "Antonio Alves",
+            email: "antonio@rentx.com.br",
+            password: "1234",
+            driver_license: "1234123411",
+        };
+
+        usersRepositoryInMemory.create(user);
+    });
+});
