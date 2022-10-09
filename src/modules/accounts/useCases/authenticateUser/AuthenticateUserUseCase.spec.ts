e22425ff628402c708f14e969192024a55c7e5b6
Antonio Alves <antonioaugusto.alves1981@gmail.com>
2022-10-09 11:46:20 -0300

    Testes unitários não funcionam após a refatoração

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,8 +1,9 @@
 import { ICreateUserDTO } from "@modules/accounts/dtos/ICreateUserDTO";
 import { UsersRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersRepositoryInMemory";
-import { AuthenticateUserUseCase } from "@modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase";
-import { CreateUserUseCase } from "@modules/accounts/useCases/createUser/CreateUserUseCase";
 import { AppError } from "@shared/errors/AppError";
+
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
 
 let createUserUseCase: CreateUserUseCase;
 let autheticateUserUseCase: AuthenticateUserUseCase;
