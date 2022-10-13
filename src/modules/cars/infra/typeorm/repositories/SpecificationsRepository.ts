7007f7a3cd29b196574db4ebf3258cee36766129
Antonio Alves <antonioaugusto.alves1981@gmail.com>
2022-10-13 15:02:36 -0300

    Não salva dentro do banco de dados

--- src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
@@ -31,8 +31,9 @@ class SpecificationsRepository implements ISpecificationsRepository {
         return specification;
     }
 
-    findByIds(ids: string[]): Promise<Specification[]> {
-        throw new Error("Method not implemented.");
+    async findByIds(ids: string[]): Promise<Specification[]> {
+        const specifications = await this.repository.findByIds(ids);
+        return specifications;
     }
 }
 
